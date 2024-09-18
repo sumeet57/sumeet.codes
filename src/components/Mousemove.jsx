4a2370ec4d5f63b8477e016f81a586d41e8ed24d
sumeet57 <sumeetumbalkar57@gmail.com>
2024-09-18 23:21:30 +0530

    responsive navbar

--- src/components/Mousemove.jsx
+++ src/components/Mousemove.jsx
@@ -1,46 +1,32 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 const Mousemove = () => {
   const cursorRef = useRef(null);
-  const cursorRef2 = useRef(null);
-  
 
   useEffect(() => {
     // Animate the cursor with mouse movement
     const handleMouseMove = (e) => {
       gsap.to(cursorRef.current, {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.9,
-        // ease: "power2.out",
-      });
-      gsap.to(cursorRef2.current, {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.8,
-        // ease: "power2.out",
+        x: e.clientX - 12,
+        y: e.clientY - 12,
+        duration: 0.5,
+        ease: "power2.inout",
       });
     };
 
-    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
   return (
     <div
-  ref={cursorRef}
-  className="w-[30px] h-[30px] border-[1px] border-white rounded-3xl fixed flex justify-center items-center"
->
-  <div
-    ref={cursorRef2}
-    className="fixed w-[2px] h-[2px] bg-white rounded-3xl"
-  >
-  </div>
-</div>
+      ref={cursorRef}
+      className="w-[30px] h-[30px] z-[3] border-[1px] border-white rounded-3xl fixed flex justify-center items-center"
+    ></div>
   );
 };
 
--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,48 +1,79 @@
-import React from 'react'
-import '../../stylesheet/header.css';
-import { RiFacebookLine, RiGithubLine, RiInstagramLine, RiLinkedinLine, RiMenuLine } from "react-icons/ri";
-
+import React, { useEffect, useState } from "react";
+import { gsap } from "gsap";
+import "../../stylesheet/header.css";
+import {
+  RiCloseLine,
+  RiFacebookLine,
+  RiGithubLine,
+  RiInstagramLine,
+  RiLinkedinLine,
+  RiMenuLine,
+} from "react-icons/ri";
 
 const Header = () => {
+  const [flag, setFlag] = useState(false);
+
+  const openMenu = () => {
+    if (!flag) {
+      gsap.to(".menu", {
+        duration: 0.5,
+        transform: "translateY(0%) rotate(0deg)",
+        opacity: 1,
+        ease: "power2.inout",
+      });
+      setFlag(true);
+    } else {
+      gsap.to(".menu", {
+        duration: 0.5,
+        opacity: 0,
+        transform: "translateY(-130%) rotate(-20deg)",
+        ease: "power2.inout",
+      });
+      setFlag(false);
+    }
+  };
+
   return (
     <>
-    <header>
-      <nav>
-        <div className="navcont">
-          <div className="navleft">
-                <RiMenuLine />
-          </div>
-          <div className="navright">
-            <i className="ri-github-line g">
+      <header>
+        <nav>
+          <div className="navcont">
+            <div onClick={openMenu} className="navleft">
+              <RiMenuLine />
+            </div>
+            <div className="navright">
+              <i className="ri-github-line g">
                 <RiGithubLine />
-            </i>
-            <i className="ri-facebook-line f">
+              </i>
+              <i className="ri-facebook-line f">
                 <RiFacebookLine />
-            </i>
-            <i className="ri-linkedin-line l">
-            <RiLinkedinLine />
-            </i>
-            <i className="ri-instagram-line i">
+              </i>
+              <i className="ri-linkedin-line l">
+                <RiLinkedinLine />
+              </i>
+              <i className="ri-instagram-line i">
                 <RiInstagramLine />
-            </i>
+              </i>
+            </div>
           </div>
-        </div>
-        <div className="menu">
-          <div className="menuicon">
-            <i className="ri-close-line close"></i>
+          <div className="menu">
+            <div
+              onClick={openMenu}
+              className="close absolute right-0 top-0 p-2 bg-slate-400 m-2 rounded-full bg-[var(--main)] text-5xl"
+            >
+              <RiCloseLine />
+            </div>
+            <div className="left w-[40%] h-screen flex justify-center flex-col bg-slate-400 text-left">
+              <p className="text-6xl px-2 py-10">home</p>
+              <p className="text-6xl px-2 py-10">home</p>
+              <p className="text-6xl px-2 py-10">home</p>
+            </div>
+            <div className="right"></div>
           </div>
-          <div className="menubody">
-            <h1>home</h1>
-            <h1>work</h1>
-            <h1>project</h1>
-            <h1>resume</h1>
-            <h1>contact</h1>
-          </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
